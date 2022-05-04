240e43e40e7b936869baffca5a36b72d869c6ef8
Wesley Rodrigues dos Reis <wesleyepolly16@gmail.com>
2022-05-04 17:19:36 -0300

    instalação do cors

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,8 +1,14 @@
+import cors from 'cors';
 import express from 'express'
 import { routes } from './routes';
 
 const app = express();
 
+app.use(
+  cors({
+    origin: "http://localhost:3000",
+  })
+);
 app.use(express.json());
 app.use(routes)
 
